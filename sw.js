f31c3ebd4a42147f4bf11764c4b85571ac530e15
FarmasiStikesAdila <mayraaditama01@gmail.com>
2025-05-14 06:24:53 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,20 @@
-// Service Worker Sederhana - FarmasiStikesAdila
-self.addEventListener('install', (event) => {
-  console.log('[SW] Installed');
-  event.waitUntil(self.skipWaiting());
+// Versi final yang sudah di-test
+self.addEventListener('install', (e) => {
+  console.log('[SW] Installed!');
+  e.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('[SW] Activated');
-  event.waitUntil(self.clients.claim());
+self.addEventListener('activate', (e) => {
+  console.log('[SW] Activated!');
+  e.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('push', (event) => {
-  console.log('[SW] Push Received');
-  const options = {
-    body: 'Waktunya minum obat sesuai jadwal!',
-    // icon: 'icon.png',  // Sementara di-disable karena file tidak ada
-    vibrate: [200, 100, 200]  // Tambahkan getar sebagai alternatif
-  };
-  event.waitUntil(
-    self.registration.showNotification('Pengingat Obat', options)
-  );
-});
-
-self.addEventListener('notificationclick', (event) => {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow('https://farmasistikesadila.github.io')
+self.addEventListener('push', (e) => {
+  console.log('[SW] Memicu Notifikasi');
+  e.waitUntil(
+    self.registration.showNotification('Pengingat Obat', {
+      body: 'Waktunya minum obat sesuai jadwal!',
+      vibrate: [200,100,200]
+    })
   );
 });
